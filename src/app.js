7bb434d80599edac6c08f6373a16c49adbe57fd0
SpookyPinkyTux <sandrotutberidze18@gmail.com>
2021-07-09 10:57:10 +0400

    Json parsing added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,59 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const path = require('path')
+const { writeFileSync, readFileSync, readFile, writeFile } = require('fs')
+const { rejects } = require('assert')
+
+async function emptyJson(fileName)
+{
+    return new Promise((resolve, reject) =>
+    {
+        await readFile(fileName, (err, data) => 
+        {
+            if (err)
+            {
+                reject(err)
+            }
+
+            else
+            {
+                let file_data = 
+                {
+                    size : data.length,
+                    content : data
+                }
+
+                resolve(file_data)    
+            }
+        })
+    })
+}
 
 app.listen(5000)
 
-app.use('/static', express.static(path.join(__dirname, '..', 'public')))
+app.use('/public', express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) =>
 {   
-    res.status(200).sendFile(path.join(__dirname, '..', 'img', 'pentle.png'))
+    res.status(200).sendFile(path.join(__dirname, '..', 'public/startPage.html'))
+})
+
+app.get('/user/:user', (req, res) =>
+{
+    emptyJson('users/users.json').then(data => 
+    {
+        //TODO
+    })
+
+    readFileSync('users/users.json', buffer)
+
+    const users = JSON.parse(buffer)
+
+    users.name.push(req.params.user)
+
+    const data = JSON.stringify(users)
+
+    writeFileSync('users/users.json', data)
+
+    res.send('user successfully registered!').status(200)
 })
